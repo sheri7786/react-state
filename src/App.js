43cb41c491cedf12e8c846128743d9b3d27b39ab
sheri7786 <66200396+sheri7786@users.noreply.github.com>
2020-06-17 00:13:21 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Room from "./room";
+
 function App() {
   return (
-   <div>
-     <Room></Room>
-   </div>
+    <div>
+      <Room></Room>
+    </div>
   );
 }
 
--- src/room.js
+++ src/room.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./room.css";
+import on from './assets/bulb1.png';
+import off from './assets/bulb.png';
 
 function Room() {
-  return (
-   <div>
-       this is room component 
-   </div>
-  );
+    let [isLit, setLit] = useState(false);
+    let [age, setAge] = useState(22);
+    let [light, setLight] = useState(false);
+    let [temp, setTemp] = useState(22);
+    return (
+        <div>
+            <div className={`room  ${isLit ? "lit" : "dark"}`}>
+                this room is: {isLit ? "lit" : "dark"}
+                <br />
+                Age: {age}
+                <br />
+                <button onClick={() => setLit(!isLit)}>toggle button</button>
+                <br />
+                <button onClick={() => {
+                    setAge(++age);
+                }}>increase in age</button>
+                <br />
+            </div>
+            <div>
+                <img src={`${light ? on : off}`} alt="bulb" />
+                {/* light is : {light ? "on" : "off"}; */}
+                <br />
+                <button onClick={() => setLight(true)}>ON</button>
+                <button onClick={() => setLight(false)}>Off</button>
+            </div><br />
+            <div>
+                Temprature in Celsius : {temp}
+                <br />
+                <button onClick={() => setTemp(++temp)}>+</button>
+                <button onClick={() => setTemp(--temp)}>-</button>
+            </div>
+        </div>
+
+    );
 }
 
 export default Room;
